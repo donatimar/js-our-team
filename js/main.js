c71f0b7ec11afd60ff508816c2a0204e5f377664
donatimar <donatimar@gmail.com>
2024-10-29 14:49:54 +0100

    bonus: add new team member

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Array dei membri iniziali del team
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -37,25 +38,48 @@ const teamMembers = [
   },
 ];
 
+// Genero e mostro le card di tutti i membri del team
 const cardsContainerEl = document.getElementById("cards-container");
 
-for (let index = 0; index < teamMembers.length; index++) {
-  const element = teamMembers[index];
-  cardsContainerEl.innerHTML += `
-      <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
-          <div class="my_card">
-              <img class="img-fluid card-image mb-3" src="${element.img}" alt="foto di ${element.name}">
-              <div class="card-description p-2">
-                  <h3 class="card-name mb-1">
-                      ${element.name}
-                  </h3>
-                  <p class="card-role mb-1">
-                      ${element.role}
-                  </p>
-                  <p class="card-email mb-3">
-                      ${element.email}    
-                  </p>
-              </div>
-          </div>
-      </div>`;
+function displayTeamMembers() {
+  cardsContainerEl.innerHTML = "";
+
+  for (let index = 0; index < teamMembers.length; index++) {
+    const element = teamMembers[index];
+    cardsContainerEl.innerHTML += `
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+            <div class="my_card">
+                <img class="img-fluid card-image mb-3" src="${element.img}" alt="foto di ${element.name}">
+                <div class="card-description p-2">
+                    <h3 class="card-name mb-1">${element.name}</h3>
+                    <p class="card-role mb-1">${element.role}</p>
+                    <p class="card-email mb-3">${element.email}</p>
+                </div>
+            </div>
+        </div>`;
+  }
 }
+
+displayTeamMembers();
+
+// Aggiungo un nuovo membro
+document.getElementById("add-member-btn").addEventListener("click", () => {
+  const name = document.getElementById("name").value;
+  const role = document.getElementById("role").value;
+  const email = document.getElementById("email").value;
+
+  if (name && role && email) {
+    teamMembers.push({
+      name: name,
+      role: role,
+      email: email,
+      img: "",
+    });
+
+    displayTeamMembers();
+
+    document.getElementById("add-member-form").reset();
+  } else {
+    alert("CSompila tutti i campi");
+  }
+});
